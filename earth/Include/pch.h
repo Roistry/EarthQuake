2f1e51f518f63089006f97f108de8fc1ec9ec667
Akuro409 <akuro4090@outlook.com>
2023-03-15 17:52:33 -0700

    "Refactored Game Classes"

--- earth/Include/pch.h
+++ earth/Include/pch.h
@@ -12,10 +12,16 @@
 
 #include "Feature/FeatureBase/FeatureBase.h"
 #include "Feature/FeatureManager/FeatureManager.h"
+
 #include "Detour-Hook/x86Detour/x86Detour.h"
 #include "Detour-Hook/x86Hook/x86Hook.h"
+
+#include "Game classes/utils.h"
 #include "Game classes/reclass.h"
+#include "Game classes/SDK.h"
+
 #include "Opengl-Imgui/Draw/glDraw.h"
 #include "Opengl-Imgui/WindowProedure/WindowProcedureHook.h"
 #include "Opengl-Imgui/Opengl/OpenglHook.h"
+
 #include "Globals/globals.h"
